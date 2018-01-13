edb9609b03fa7ca6c30fb079721ef2b368162883
Sophia Song <sophia.song@tufts.edu>
2018-01-13 18:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // write your code below!
+function happyHolidays (happyHolidays, element) {return string ("Happy Holidays!")}
+function happyHolidayTo (christmas, will) {return string (`Happy ${holiday}, ${name}!`)}
+function holidayCountdown (holiday, number) {return string `It's ${days} days until ${holiday}!`}
+
